25b16c98c6cf58b868a54c23937bb0b323579c8e
kavinsagar532 <kavinsagar532@gmail.com>
2023-11-06 12:08:56 +0530

    third

--- app.py
+++ app.py
@@ -4,30 +4,37 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Skin detection parameters
-lower = np.array([0, 48, 80], dtype="uint8")
-upper = np.array([20, 255, 255], dtype="uint8")
+def detect_skin():
+    # define the upper and lower boundaries of the HSV pixel intensities to be considered 'skin'
+    lower = np.array([0, 48, 80], dtype="uint8")
+    upper = np.array([20, 255, 255], dtype="uint8")
 
-def detect_skin(frame):
-    converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    skinMask = cv2.inRange(converted, lower, upper)
-
-    # Apply processing steps (erosion, dilation, etc.) to skinMask here
-    # ...
-
-    return skinMask
-
-def webcam():
     camera = cv2.VideoCapture(0)
+
     while True:
-        _, frame = camera.read()
-        skin_mask = detect_skin(frame)
-        frame_with_skin = cv2.bitwise_and(frame, frame, mask=skin_mask)
-        frame_with_skin = cv2.cvtColor(frame_with_skin, cv2.COLOR_BGR2RGB)
-        _, jpeg = cv2.imencode('.jpg', frame_with_skin)
-        frame_bytes = jpeg.tobytes()
+        (grabbed, frame) = camera.read()
+
+        if not grabbed:
+            break
+
+        converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        skinMask = cv2.inRange(converted, lower, upper)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+        skinMask = cv2.erode(skinMask, kernel, iterations=2)
+        skinMask = cv2.dilate(skinMask, kernel, iterations=2)
+
+        skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+        skin = cv2.bitwise_and(frame, frame, mask=skinMask)
+
+        _, jpeg = cv2.imencode('.jpg', np.hstack([frame, skin]))
+        frame = jpeg.tobytes()
+
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    camera.release()
+    cv2.destroyAllWindows()
 
 @app.route('/')
 def index():
@@ -35,7 +42,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(webcam(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(detect_skin(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
